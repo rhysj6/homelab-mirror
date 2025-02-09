dcdc2243b5defd0541657fc178679fb1fb564d76
rhysj6 <rhysj6@gmail.com>
2025-02-09 23:03:22 +0000

    Add Longhorn Helm release configuration

---
+++ modules/core_cluster/longhorn.tf
@@ -0,0 +1,30 @@
+resource "kubernetes_namespace" "longhorn" {
+  metadata {
+    name = "longhorn-system"
+  }
+}
+
+resource "helm_release" "longhorn" {
+  chart       = "longhorn"
+  repository  = "https://charts.longhorn.io"
+  name        = "longhorn"
+  namespace   = kubernetes_namespace.longhorn.id
+  version     = "1.8.0"
+  max_history = 2
+  set {
+    name  = "ingress.enabled"
+    value = true
+  }
+  set {
+    name  = "ingress.annotations.cert-manager\\.io/cluster-issuer"
+    value = resource.kubernetes_manifest.cluster_issuer.manifest.metadata.name
+  }
+  set {
+    name  = "ingress.tls"
+    value = true
+  }
+  set {
+    name  = "ingress.host"
+    value = "longhorn.${data.infisical_secrets.kubernetes.secrets["cluster_subdomain"].value}"
+  }
+}
