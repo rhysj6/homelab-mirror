8fc0fd9325d5b1204dd53465ad6b0168dea1bce3
rhysj6 <rhysj6@gmail.com>
2025-02-11 23:33:43 +0000

    Removed needless dependency definitions

--- clusters/management/core/providers.tf
+++ clusters/management/core/providers.tf
@@ -1,39 +1,11 @@
-terraform {
+# terraform {
   # cloud {
   #   organization = "Homelab"
   #   workspaces {
   #     name = "Rancher_Cluster"
   #   }
   # }
-
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.0.0"
-    }
-    infisical = {
-      version = ">= 0.13.0"
-      source  = "infisical/infisical"
-    }
-    authentik = {
-      source  = "goauthentik/authentik"
-      version = "2024.6.1" ## TODO: Update this when redeploying authentik.
-    }
-    minio = {
-      source  = "aminueza/minio"
-      version = "3.2.3"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.3"
-    }
-
-  }
-}
+# }
 provider "kubernetes" {
   // Handled in the environment variables.
   config_path = "/workspaces/homelab/management_kubeconfig"
