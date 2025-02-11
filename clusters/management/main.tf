0ce99be6470db8d5e7facc9cc01525f5e6a20bd9
rhysj6 <rhysj6@gmail.com>
2025-02-11 00:48:53 +0000

    Add management cluster configuration in main.tf

---
+++ clusters/management/main.tf
@@ -0,0 +1,7 @@
+module "core" {
+    source = "../../modules/core_cluster"
+    cluster_name = "management"
+    main_node_ip = "10.20.10.11"
+    cilium_loadbalancer_ip_pool_cidr = "10.20.11.1/24"
+    ingress_controller_ip = "10.20.11.11"
+}
