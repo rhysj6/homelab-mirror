a08a1b0151a64fdc42a20a0ec5d48bea23e2e09a
rhysj6 <rhysj6@gmail.com>
2025-02-11 00:48:00 +0000

    Add Traefik Helm release and namespace configuration

---
+++ modules/core_cluster/traefik.tf
@@ -0,0 +1,53 @@
+resource "kubernetes_namespace" "traefik" {
+  metadata {
+    name = "traefik"
+  }
+}
+
+resource "helm_release" "traefik_crds" {
+  chart       = "traefik-crds"
+  repository  = "https://traefik.github.io/charts"
+  name        = "traefik-crds"
+  namespace   = kubernetes_namespace.traefik.id
+  version     = "1.3.0"
+  max_history = 2
+}
+
+
+resource "helm_release" "traefik" {
+  chart       = "traefik"
+  repository  = "https://traefik.github.io/charts"
+  name        = "traefik"
+  namespace   = kubernetes_namespace.traefik.id
+  version     = "34.3.0"
+  max_history = 2
+  depends_on  = [helm_release.traefik_crds, kubernetes_namespace.traefik]
+  set_list {
+    name  = "additionalArguments"
+    value = ["--serversTransport.insecureSkipVerify=true"]
+  }
+  set {
+    name  = "ports.websecure.asDefault"
+    value = true
+  }
+  set {
+    name  = "port.swebsecure.tls.enabled"
+    value = true
+  }
+  set {
+    name  = "providers.kubernetesCRD.allowExternalNameServices"
+    value = true
+  }
+  set {
+    name  = "providers.kubernetesIngress.allowExternalNameServices"
+    value = true
+  }
+  set {
+    name  = "service.annotations.lbipam\\.cilium\\.io/ips"
+    value = var.ingress_controller_ip
+  }
+  set {
+    name = "ingressRoute.dashboard.enabled"
+    value = true
+  }
+}
