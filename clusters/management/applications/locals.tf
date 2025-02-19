aeb2a22870f214b81afc3097523307b5b2fc68a3
rhysj6 <rhysj6@gmail.com>
2025-02-19 21:11:54 +0000

    Update environment variable to reflect production readiness

--- clusters/management/applications/locals.tf
+++ clusters/management/applications/locals.tf
@@ -1,6 +1,6 @@
 
 locals {
-  environment = "redcliff" // TODO: Change this to management when moving to "production"
+  environment = "management"
   domain = data.infisical_secrets.bootstrap_secrets.secrets["domain"].value
   cluster_subdomain = data.infisical_secrets.kubernetes_secrets.secrets["cluster_subdomain"].value
   authentik_host = data.infisical_secrets.bootstrap_secrets.secrets["authentik_hostname"].value
