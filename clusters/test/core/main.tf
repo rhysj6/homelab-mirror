2e01533cf7863a5dd05a030feef6582ee777844a
rhysj6 <rhysj6@gmail.com>
2025-02-21 00:31:59 +0000

    Adjusted core module variables

--- clusters/test/core/main.tf
+++ clusters/test/core/main.tf
@@ -14,11 +14,9 @@ module "cluster" {
 }
 
 module "core" {
-  count                            = 0
+  count                            = module.test_cluster_config.cluster_exists ? 1 : 0
   source                           = "../../../modules/core_cluster"
   cluster_name                     = "management"
-  main_node_ip                     = "10.20.10.11"
-  cilium_loadbalancer_ip_pool_cidr = "10.20.11.1/24"
-  ingress_controller_ip            = "10.20.11.11"
-  number_of_nodes                  = 1
+  cilium_loadbalancer_ip_pool_cidr = "10.20.31.1/24"
+  ingress_controller_ip            = "10.20.31.11"
 }
