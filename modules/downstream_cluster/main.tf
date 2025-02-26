deabdb00ab3c4d6c57513ec9cab41c70a0f6db97
rhysj6 <rhysj6@gmail.com>
2025-02-26 00:23:27 +0000

    Fix MinIO endpoint configuration and update kernel protection settings

--- modules/downstream_cluster/main.tf
+++ modules/downstream_cluster/main.tf
@@ -18,7 +18,7 @@ resource "rancher2_cluster_v2" "cluster" {
       snapshot_schedule_cron = "0 */5 * * *"
       snapshot_retention     = 5
       s3_config {
-        endpoint              = "https://${data.infisical_secrets.bootstrap_secrets.secrets["minio_endpoint"].value}"
+        endpoint              = data.infisical_secrets.bootstrap_secrets.secrets["minio_endpoint"].value
         bucket                = minio_s3_bucket.etcd.bucket
         cloud_credential_name = rancher2_cloud_credential.minio_etc_bucket.id
       }
@@ -56,8 +56,7 @@ resource "rancher2_cluster_v2" "cluster" {
     })
     machine_selector_config {
       config = yamlencode({
-        profile = "cis"
-        protect-kernel-defaults = true
+        protect-kernel-defaults = false
       })
     }
   }
