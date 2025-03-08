bcce5e3c3eafea86be0d7225aa4342db1846d7b6
rhysj6 <rhysj6@gmail.com>
2025-03-08 01:48:00 +0000

    Update Terraform configuration to use S3 backend for state management

--- clusters/test/core/providers.tf
+++ clusters/test/core/providers.tf
@@ -1,13 +1,14 @@
-# terraform {
-# cloud {
-#   organization = "Homelab"
-#   workspaces {
-#     name = "Rancher_Cluster"
-#   }
-# }
-# }
 terraform {
-
+  backend "s3" {
+    bucket = "terraform"
+    key    = "clusters/test/core/terraform.tfstate"
+    region                      = "main"
+    skip_region_validation      = true
+    skip_requesting_account_id = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    use_path_style = true
+  }
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
