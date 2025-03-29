a5b35054d7b85a6dec00021c496b73f8c2e0afb9
rhysj6 <rhysj6@gmail.com>
2025-03-29 22:58:26 +0000

    Made specific authentik module for ease of organisation

---
+++ clusters/redcliff/modules/authentik/versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.11.1"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+    rancher2 = {
+      source  = "rancher/rancher2"
+      version = "6.0.0"
+    }
+    minio = {
+      source  = "aminueza/minio"
+      version = "3.2.3"
+    }
+    infisical = {
+      version = ">= 0.13.0"
+      source  = "infisical/infisical"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+  }
+}
