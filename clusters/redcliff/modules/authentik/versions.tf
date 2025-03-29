6fcd3e895678eb6bdf3b0f44a17f8f22410f37cb
rhysj6 <rhysj6@gmail.com>
2025-03-29 23:01:58 +0000

    Remove unused dependencies from authentik module versions.tf

--- clusters/redcliff/modules/authentik/versions.tf
+++ clusters/redcliff/modules/authentik/versions.tf
@@ -5,18 +5,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
-    rancher2 = {
-      source  = "rancher/rancher2"
-      version = "6.0.0"
-    }
-    minio = {
-      source  = "aminueza/minio"
-      version = "3.2.3"
-    }
-    infisical = {
-      version = ">= 0.13.0"
-      source  = "infisical/infisical"
-    }
     random = {
       source  = "hashicorp/random"
       version = "3.6.3"
