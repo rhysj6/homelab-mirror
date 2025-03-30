40c907b0eb1ff8a3ec21df9f4f7015a265ec0815
rhysj6 <rhysj6@gmail.com>
2025-03-30 23:48:47 +0000

    Removed dns from test as no longer needed

--- clusters/test/applications/dns.tf
+++
@@ -1,6 +0,0 @@
-module "dns" {
-    source = "../../redcliff/modules/pihole"
-    domain = data.infisical_secrets.bootstrap.secrets["domain"].value
-    load_balancer_ip = "10.20.31.53"
-    windows_domain = data.infisical_secrets.bootstrap.secrets["windows_domain"].value
-}
