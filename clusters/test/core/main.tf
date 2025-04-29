d84308d59b0a91fbb31bcde38f7a26dbd97fb0c7
rhysj6 <rhysj6@gmail.com>
2025-04-29 20:19:57 +0000

    Added bgp support for test cluster

--- clusters/test/core/main.tf
+++ clusters/test/core/main.tf
@@ -1,17 +1,19 @@
 module "core" {
   source                           = "../../../modules/core_cluster"
   cluster_name                     = "test"
-  cilium_loadbalancer_ip_pool_cidr = "10.20.31.1/24"
-  ingress_controller_ip            = "10.20.31.11"
-  monitoring_ip                    = "10.20.31.12"
+  cilium_loadbalancer_ip_pool_cidr = "10.21.30.1/24"
+  ingress_controller_ip            = "10.21.30.11"
+  monitoring_ip                    = "10.21.30.12"
+  cilium_use_bgp                   = true
+  cilium_bgp_asn                   = 65553
   cluster_node_ips = [
     "10.20.30.11",
     "10.20.30.12",
     "10.20.30.13"
   ]
-  domain = var.domain
-  cloudflare_email = var.cloudflare_email
-  cloudflare_api_key  = var.cloudflare_api_key
+  domain             = var.domain
+  cloudflare_email   = var.cloudflare_email
+  cloudflare_api_key = var.cloudflare_api_key
 }
 
 variable "domain" {
