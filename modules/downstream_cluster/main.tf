201fb646cecaedf3db2781352442b7c847b19e5f
rhysj6 <rhysj6@gmail.com>
2025-04-30 23:49:30 +0000

    Fixed issue with s3 backup

--- modules/downstream_cluster/main.tf
+++ modules/downstream_cluster/main.tf
@@ -7,7 +7,7 @@ resource "rancher2_cluster_v2" "cluster" {
       snapshot_schedule_cron = "0 */5 * * *"
       snapshot_retention     = 5
       s3_config {
-        endpoint              = "https://s3.hl.${var.domain}"
+        endpoint              = "s3.hl.${var.domain}"
         bucket                = minio_s3_bucket.etcd.bucket
         cloud_credential_name = rancher2_cloud_credential.minio_etc_bucket.id
       }
