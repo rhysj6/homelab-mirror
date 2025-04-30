87ec5083ac0c47df086a1f08cd7f0f1c02692653
rhysj6 <rhysj6@gmail.com>
2025-04-30 22:11:39 +0000

    Update load balancer IP for pihole module

--- applications/pihole/env/redcliff/main.tf
+++ applications/pihole/env/redcliff/main.tf
@@ -2,5 +2,5 @@ module "pihole" {
   source           = "../../module"
   domain           = var.domain
   windows_domain   = var.windows_domain
-  load_balancer_ip = "10.20.1.53"
+  load_balancer_ip = "10.21.0.53"
 }
