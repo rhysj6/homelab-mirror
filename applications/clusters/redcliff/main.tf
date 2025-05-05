96938273d799f066b5b1a725ed834b2762487b7c
rhysj6 <rhysj6@gmail.com>
2025-05-05 19:24:06 +0000

    Moved pihole to new structure

--- applications/clusters/redcliff/main.tf
+++ applications/clusters/redcliff/main.tf
@@ -4,3 +4,10 @@ module "gateway" {
     secondary_domain = var.secondary_domain
     windows_domain = var.windows_domain
 }
+
+module "pihole" {
+  source           = "../../modules/pihole"
+  domain           = var.domain
+  windows_domain   = var.windows_domain
+  load_balancer_ip = "10.21.0.53"
+}
--- applications/pihole/env/redcliff/main.tf
+++
@@ -1,6 +0,0 @@
-module "pihole" {
-  source           = "../../module"
-  domain           = var.domain
-  windows_domain   = var.windows_domain
-  load_balancer_ip = "10.21.0.53"
-}
