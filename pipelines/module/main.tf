a1297da0e817f0df227100f7c3946111d45c1c57
rhysj6 <rhysj6@gmail.com>
2025-05-16 13:46:52 +0000

    Added pipeline provisioner pipeline to terraform module

--- pipelines/module/main.tf
+++ pipelines/module/main.tf
@@ -6,3 +6,15 @@ data "azuredevops_serviceendpoint_github" "rhysj6" {
   project_id            = data.azuredevops_project.private.id
   service_endpoint_name = "rhysj6"
 }
+
+resource "azuredevops_build_definition" "pipeline_provisioner" {
+  project_id = data.azuredevops_project.private.id
+  name       = "Pipeline Provisioner"
+
+  repository {
+    repo_type             = "GitHub"
+    repo_id               = "rhysj6/Homelab"
+    yml_path              = "pipelines/provisioning.yml"
+    service_connection_id = data.azuredevops_serviceendpoint_github.rhysj6.id
+  }
+}
