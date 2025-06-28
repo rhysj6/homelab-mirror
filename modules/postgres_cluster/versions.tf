cb709b9a4403d4e45f34a6a9244185db5424a893
rhysj6 <rhysj6@gmail.com>
2025-06-28 23:29:06 +0000

    Removed redundant providers

--- modules/postgres_cluster/versions.tf
+++ modules/postgres_cluster/versions.tf
@@ -5,14 +5,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.0.0"
-    }
-    authentik = {
-      source  = "goauthentik/authentik"
-      version = "2025.4.0" ## TODO: Update this when redeploying authentik.
-    }
     minio = {
       source  = "aminueza/minio"
       version = "3.5.0"
@@ -20,10 +12,6 @@ terraform {
     random = {
       source  = "hashicorp/random"
       version = "3.7.2"
-    }
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 5"
     }
   }
 }
