4e8becc05761c577d42570c26809b46feef4a599
rhysj6 <rhysj6@gmail.com>
2025-06-29 16:45:05 +0000

    Adjusted version constraint to not use 3.0.2

--- clusters/test/core/providers.tf
+++ clusters/test/core/providers.tf
@@ -17,7 +17,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.0.0"
+      version = ">= 2.0.0, < 3.0.0"
     }
     minio = {
       source  = "aminueza/minio"
