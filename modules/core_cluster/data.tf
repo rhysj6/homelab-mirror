71c918ecf0ce6d8dc91a0c09a9e2d8b5f23624a5
rhysj6 <rhysj6@gmail.com>
2025-07-08 22:24:36 +0000

    Added middlewares for local only and authentik

--- modules/core_cluster/data.tf
+++ modules/core_cluster/data.tf
@@ -2,6 +2,7 @@ locals {
     cilium_crd_exists = length(data.kubernetes_resources.cilium_crds.objects) > 0
     prometheus_crd_exists = length(data.kubernetes_resources.prometheus_crds.objects) > 0
     cert_manager_crd_exists = length(data.kubernetes_resources.cluster-issuer-crd.objects) > 0
+    traefik_crd_exists = length(data.kubernetes_resources.traefik_crds.objects) > 0
 }
 
 data "kubernetes_resources" "cilium_crds" { ## Get the CRD if it exists
@@ -22,5 +23,13 @@ data "kubernetes_resources" "cluster-issuer-crd" {
   api_version    = "apiextensions.k8s.io/v1"
   kind           = "CustomResourceDefinition"
   field_selector = "metadata.name=clusterissuers.cert-manager.io"
+  limit          = 1
+}
+
+
+data "kubernetes_resources" "traefik_crds" {
+  api_version    = "apiextensions.k8s.io/v1"
+  kind           = "CustomResourceDefinition"
+  field_selector = "metadata.name=middlewares.traefik.io"
   limit          = 1
 }
--- modules/core_cluster/traefik.tf
+++ modules/core_cluster/traefik.tf
@@ -25,7 +25,55 @@ resource "helm_release" "traefik" {
   values = [
     templatefile("${path.module}/templates/traefik_values.yaml", {
       cilium_crds = data.kubernetes_resources.cilium_crds.objects
-      service_ip        = var.ingress_controller_ip
+      service_ip  = var.ingress_controller_ip
     })
   ]
+}
+
+resource "kubernetes_manifest" "local_only_middleware" {
+  manifest = {
+    apiVersion = "traefik.io/v1alpha1"
+    kind       = "Middleware"
+    metadata = {
+      name      = "local-only"
+      namespace = kubernetes_namespace.traefik.id
+    }
+    spec = {
+      ipAllowList = {
+        sourceRange = ["10.0.0.0/8"]
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_manifest" "authentik_middleware" {
+  manifest = {
+    apiVersion = "traefik.io/v1alpha1"
+    kind       = "Middleware"
+    metadata = {
+      name      = "authentik"
+      namespace = kubernetes_namespace.traefik.id
+    }
+    spec = {
+      forwardAuth = {
+        address            = "https://hl.${var.domain}/outpost.goauthentik.io/auth/traefik"
+        trustForwardHeader = true
+        authResponseHeaders = [
+          "X-authentik-username",
+          "X-authentik-groups",
+          "X-authentik-entitlements",
+          "X-authentik-email",
+          "X-authentik-name",
+          "X-authentik-uid",
+          "X-authentik-jwt",
+          "X-authentik-meta-jwks",
+          "X-authentik-meta-outpost",
+          "X-authentik-meta-provider",
+          "X-authentik-meta-app",
+          "X-authentik-meta-version"
+        ]
+      }
+    }
+  }
 }
