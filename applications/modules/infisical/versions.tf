6775bb0ef1dd5969865f8960d62ee4dead1dee4c
rhysj6 <rhysj6@gmail.com>
2025-09-06 19:58:18 +0000

    Moved to pinned version number for kubernetes provider

--- applications/modules/infisical/versions.tf
+++ applications/modules/infisical/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
+      version = "2.38.0"
     }
     authentik = {
       source  = "goauthentik/authentik"
