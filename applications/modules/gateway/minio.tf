abdddeefaa55cf50393a2bbf1f73f102bfcc7aff
rhysj6 <rhysj6@gmail.com>
2025-09-14 20:52:00 +0100

    Updated ingresses

--- applications/modules/gateway/minio.tf
+++ applications/modules/gateway/minio.tf
@@ -1,11 +1,11 @@
-resource "kubernetes_service_v1" "clifton_minio" {
+resource "kubernetes_service_v1" "minio_1" {
   metadata {
-    name      = "clifton-minio"
+    name      = "minio-1"
     namespace = kubernetes_namespace.external_hosts.metadata[0].name
   }
   spec {
     type          = "ExternalName"
-    external_name = "10.10.0.131"
+    external_name = "10.10.0.141"
     port {
       port        = 9000
       target_port = 9000
@@ -19,14 +19,14 @@ resource "kubernetes_service_v1" "clifton_minio" {
   }
 }
 
-resource "kubernetes_service_v1" "filton_minio" {
+resource "kubernetes_service_v1" "minio_2" {
   metadata {
-    name      = "filton-minio"
+    name      = "minio-2"
     namespace = kubernetes_namespace.external_hosts.metadata[0].name
   }
   spec {
     type          = "ExternalName"
-    external_name = "10.10.0.132"
+    external_name = "10.10.0.142"
     port {
       port        = 9000
       target_port = 9000
@@ -40,13 +40,80 @@ resource "kubernetes_service_v1" "filton_minio" {
   }
 }
 
-resource "kubernetes_ingress_v1" "minio_main_ingress" { # Going to have some instance specific ingress resources
+resource "kubernetes_ingress_v1" "minio_s3_ingress" { # Going to have some instance specific ingress resources
   # This is the main ingress resource for minio
   metadata {
-    name      = "minio-ingress"
+    name      = "minio-s3-ingress"
     namespace = "external-hosts"
     annotations = {
       "cert-manager.io/cluster-issuer" = "cert-manager"
+    }
+  }
+  spec {
+    rule {
+      host = "s3.hl.${var.domain}"
+      http {
+        path {
+          backend {
+            service {
+              name = kubernetes_service_v1.minio_1.metadata[0].name
+              port {
+                name = "api"
+              }
+            }
+          }
+        }
+      }
+    }
+    rule {
+      host = "s3-1.hl.${var.domain}"
+      http {
+        path {
+          backend {
+            service {
+              name = kubernetes_service_v1.minio_1.metadata[0].name
+              port {
+                name = "api"
+              }
+            }
+          }
+        }
+      }
+    }
+    rule {
+      host = "s3-2.hl.${var.domain}"
+      http {
+        path {
+          backend {
+            service {
+              name = kubernetes_service_v1.minio_2.metadata[0].name
+              port {
+                name = "api"
+              }
+            }
+          }
+        }
+      }
+    }
+    tls {
+      secret_name = "minio-s3-tls"
+      hosts = [
+        "s3.hl.${var.domain}",
+        "s3-1.hl.${var.domain}",
+        "s3-2.hl.${var.domain}"
+      ]
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "minio_console_ingress" { # Going to have some instance specific ingress resources
+  # This is the main ingress resource for minio
+  metadata {
+    name      = "minio-console-ingress"
+    namespace = "external-hosts"
+    annotations = {
+      "cert-manager.io/cluster-issuer" = "cert-manager",
+      "traefik.ingress.kubernetes.io/router.middlewares" = "traefik-local-only@kubernetescrd"
     }
   }
   spec {
@@ -56,7 +123,7 @@ resource "kubernetes_ingress_v1" "minio_main_ingress" { # Going to have some ins
         path {
           backend {
             service {
-              name = kubernetes_service_v1.clifton_minio.metadata[0].name
+              name = kubernetes_service_v1.minio_1.metadata[0].name
               port {
                 name = "console"
               }
@@ -65,126 +132,10 @@ resource "kubernetes_ingress_v1" "minio_main_ingress" { # Going to have some ins
         }
       }
     }
-    rule {
-      host = "s3.hl.${var.domain}"
-      http {
-        path {
-          backend {
-            service {
-              name = kubernetes_service_v1.clifton_minio.metadata[0].name
-              port {
-                name = "api"
-              }
-            }
-          }
-        }
-      }
-    }
     tls {
-      secret_name = "minio-main-tls"
+      secret_name = "minio-console-tls"
       hosts = [
-        "minio.hl.${var.domain}",
-        "s3.hl.${var.domain}"
-      ]
-    }
-  }
-}
-
-
-resource "kubernetes_ingress_v1" "minio_clifton_ingress" { # Clifton specific ingress resource
-  metadata {
-    name      = "clifton-minio-ingress"
-    namespace = "external-hosts"
-    annotations = {
-      "cert-manager.io/cluster-issuer" = "cert-manager"
-    }
-  }
-  spec {
-    rule {
-      host = "minio.clifton.hl.${var.domain}"
-      http {
-        path {
-          backend {
-            service {
-              name = kubernetes_service_v1.clifton_minio.metadata[0].name
-              port {
-                name = "console"
-              }
-            }
-          }
-        }
-      }
-    }
-    rule {
-      host = "s3.clifton.hl.${var.domain}"
-      http {
-        path {
-          backend {
-            service {
-              name = kubernetes_service_v1.clifton_minio.metadata[0].name
-              port {
-                name = "api"
-              }
-            }
-          }
-        }
-      }
-    }
-    tls {
-      secret_name = "minio-clifton-tls"
-      hosts = [
-        "minio.clifton.hl.${var.domain}",
-        "s3.clifton.hl.${var.domain}"
-      ]
-    }
-  }
-}
-
-
-resource "kubernetes_ingress_v1" "minio_filton_ingress" { # filton specific ingress resource
-  metadata {
-    name      = "filton-minio-ingress"
-    namespace = "external-hosts"
-    annotations = {
-      "cert-manager.io/cluster-issuer" = "cert-manager"
-    }
-  }
-  spec {
-    rule {
-      host = "minio.filton.hl.${var.domain}"
-      http {
-        path {
-          backend {
-            service {
-              name = kubernetes_service_v1.filton_minio.metadata[0].name
-              port {
-                name = "console"
-              }
-            }
-          }
-        }
-      }
-    }
-    rule {
-      host = "s3.filton.hl.${var.domain}"
-      http {
-        path {
-          backend {
-            service {
-              name = kubernetes_service_v1.filton_minio.metadata[0].name
-              port {
-                name = "api"
-              }
-            }
-          }
-        }
-      }
-    }
-    tls {
-      secret_name = "minio-filton-tls"
-      hosts = [
-        "minio.filton.hl.${var.domain}",
-        "s3.filton.hl.${var.domain}"
+        "minio.hl.${var.domain}"
       ]
     }
   }
