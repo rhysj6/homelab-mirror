b5c8d8f0240fc5d2a1ea1221078a85b49ed4e430
rhysj6 <rhysj6@gmail.com>
2025-09-28 18:40:50 +0100

    Made general start on central postgresql db

---
+++ applications/postgresql/minio.tf
@@ -0,0 +1,55 @@
+resource "minio_s3_bucket" "postgres_backup" {
+  bucket = "postgresql-backup"
+  acl    = "private"
+}
+
+resource "minio_iam_policy" "postgres_backup" {
+  name = "postgresql-backup-full-access-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["s3:*"]
+        Effect   = "Allow"
+        Resource = ["arn:aws:s3:::${minio_s3_bucket.postgres_backup.bucket}/*"]
+      }
+    ]
+  })
+}
+
+resource "minio_ilm_policy" "postgres_backup" {
+  bucket = minio_s3_bucket.postgres_backup.bucket
+
+  rule {
+    id = "versioning"
+    noncurrent_expiration {
+      days = "45d"
+    }
+    expiration = "DeleteMarker"
+  }
+}
+
+resource "minio_iam_user" "postgres_backup" {
+  name = "postgresql-backup"
+}
+
+resource "minio_iam_user_policy_attachment" "postgres_backup" {
+  user_name   = minio_iam_user.postgres_backup.name
+  policy_name = minio_iam_policy.postgres_backup.name
+}
+
+resource "minio_iam_service_account" "backup" {
+  target_user = "postgresql-backup"
+}
+
+resource "kubernetes_secret_v1" "minio" {
+  metadata {
+    name      = "minio-backup"
+    namespace = "cnpg-system"
+  }
+  data = {
+    access_key        = minio_iam_service_account.backup.access_key
+    secret_access_key = minio_iam_service_account.backup.secret_key
+  }
+  type = "Opaque"
+}
---
+++ applications/postgresql/providers.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_version = ">= 1.11.1"
+  backend "s3" {
+    bucket                      = "terraform"
+    key                         = "applications/postgresql.tfstate"
+    region                      = "main"
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    use_path_style              = true
+  }
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.38.0"
+    }
+    minio = {
+      source  = "aminueza/minio"
+      version = "3.6.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
+    authentik = {
+      source  = "goauthentik/authentik"
+      version = "2025.8.1"
+    }
+  }
+}
+
+module "cluster_config" {
+  source       = "rhysj6/kubeconfig/rancher"
+  version      = "1.1.0"
+  cluster_name = "redcliff"
+}
+
+provider "kubernetes" {
+  host  = module.cluster_config.host
+  token = module.cluster_config.token
+  ignore_annotations = [
+    ".*cattle\\.io.*"
+  ]
+  ignore_labels = [
+    ".*cattle\\.io.*"
+  ]
+}
+provider "helm" {
+  kubernetes = {
+    host  = module.cluster_config.host
+    token = module.cluster_config.token
+  }
+}
+
+provider "minio" {
+  // Handled in the environment variables.
+  // MINIO_ENDPOINT
+  // MINIO_USER
+  // MINIO_PASSWORD
+  minio_ssl = true
+}
