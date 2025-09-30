dadc76acaa6427ed2b9346c43756c3790a9c9bb5
rhysj6 <rhysj6@gmail.com>
2025-09-30 23:44:32 +0100

    Readded health check now that cillium is working again

--- talos/test/main.tf
+++ talos/test/main.tf
@@ -17,21 +17,21 @@ resource "talos_machine_bootstrap" "bootstrap" {
   node                 = local.node_1_ip
 }
 
-# data "talos_cluster_health" "health" {
-#   depends_on           = [ 
-#     talos_machine_configuration_apply.cp_01_config_apply,
-#     talos_machine_configuration_apply.cp_02_config_apply,
-#     talos_machine_configuration_apply.cp_03_config_apply
-#     ]
-#   client_configuration = data.talos_client_configuration.talosconfig.client_configuration
-#   control_plane_nodes  = [ local.node_1_ip, local.node_2_ip, local.node_3_ip ]
-#   endpoints            = data.talos_client_configuration.talosconfig.endpoints
-# }
+data "talos_cluster_health" "health" {
+  depends_on           = [ 
+    talos_machine_configuration_apply.cp_01_config_apply,
+    talos_machine_configuration_apply.cp_02_config_apply,
+    talos_machine_configuration_apply.cp_03_config_apply
+    ]
+  client_configuration = data.talos_client_configuration.talosconfig.client_configuration
+  control_plane_nodes  = [ local.node_1_ip, local.node_2_ip, local.node_3_ip ]
+  endpoints            = data.talos_client_configuration.talosconfig.endpoints
+}
 
 resource "talos_cluster_kubeconfig" "kubeconfig" {
   depends_on = [
     talos_machine_bootstrap.bootstrap,
-    #    data.talos_cluster_health.health
+    data.talos_cluster_health.health
   ]
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
   node                 = local.node_1_ip
