bfc651404ba007809709ae27000c0750f36a0583
rhysj6 <rhysj6@gmail.com>
2025-10-12 21:32:09 +0100

    Fixed missing providers to use generated kubeconfig

--- clusters/test/cluster/providers.tf
+++ clusters/test/cluster/providers.tf
@@ -16,5 +16,33 @@ required_version = ">= 1.11.1"
       source = "siderolabs/talos"
       version = "0.9.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.38.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
+    infisical = {
+      source = "Infisical/infisical"
+      version = "0.15.37"
+    }
+  }
+}
+
+provider "kubernetes" {
+    host = "https://${local.kubevip}:6443"
+    cluster_ca_certificate = talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.ca_certificate
+    client_certificate = talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.client_certificate
+    client_key = talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.client_key
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = "https://${local.kubevip}:6443"
+    cluster_ca_certificate = base64decode(talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.ca_certificate)
+    client_certificate     = base64decode(talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.client_certificate)
+    client_key             = base64decode(talos_cluster_kubeconfig.kubeconfig.kubernetes_client_configuration.client_key)
   }
 }
