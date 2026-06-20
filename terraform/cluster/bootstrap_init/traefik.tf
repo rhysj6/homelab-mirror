28a09fdff1cfb33f03747cec0b904250fc0b4824
rhysj6 <rhysj6@gmail.com>
2026-06-20 23:46:36 +0100

    Removed traefik crds helm chart and updated main traefik helm chart

--- terraform/cluster/bootstrap_init/traefik.tf
+++ terraform/cluster/bootstrap_init/traefik.tf
@@ -4,24 +4,14 @@ resource "kubernetes_namespace" "traefik" {
   }
 }
 
-resource "helm_release" "traefik_crds" {
-  chart       = "traefik-crds"
-  repository  = "https://traefik.github.io/charts"
-  name        = "traefik-crds"
-  namespace   = kubernetes_namespace.traefik.id
-  version     = "1.15.0"
-  max_history = 2
-}
-
-
 resource "helm_release" "traefik" {
   chart       = "traefik"
   repository  = "https://traefik.github.io/charts"
   name        = "traefik"
   namespace   = kubernetes_namespace.traefik.id
-  version     = "39.0.8"
+  version     = "41.0.0"
   max_history = 2
-  depends_on  = [helm_release.traefik_crds, kubernetes_namespace.traefik, helm_release.kube_prometheus_stack]
+  depends_on  = [kubernetes_namespace.traefik, helm_release.kube_prometheus_stack]
   values = [
     templatefile("${path.module}/templates/traefik_values.yaml", {
       service_ip  = var.ingress_controller_ip
