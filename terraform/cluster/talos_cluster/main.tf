819daf6269545c86345e520866489fdb33d9e630
rhysj6 <rhysj6@gmail.com>
2026-07-23 00:30:57 +0100

    Removed cluster health check
    Doesn't appear to work with custom cilium native routing

--- terraform/cluster/talos_cluster/main.tf
+++ terraform/cluster/talos_cluster/main.tf
@@ -28,34 +28,13 @@ resource "talos_machine_bootstrap" "bootstrap" {
   node                 = local.control_plane_endpoints[0]
 }
 
-
-data "talos_cluster_health" "init_health" {
-  depends_on = [talos_machine_configuration_apply.control_plane, talos_machine_configuration_apply.worker]
-
-  client_configuration   = data.talos_client_configuration.talosconfig.client_configuration
-  control_plane_nodes    = local.control_plane_endpoints
-  worker_nodes           = [for n in local.worker_nodes : n.ip_address]
-  endpoints              = local.control_plane_endpoints
-  skip_kubernetes_checks = true
-}
-
 resource "time_sleep" "kubevip_wait" {
-  depends_on = [talos_machine_bootstrap.bootstrap, data.talos_cluster_health.init_health]
-  create_duration = "25s"
+  depends_on = [talos_machine_bootstrap.bootstrap]
+  create_duration = "59s"
 }
 
 resource "talos_cluster_kubeconfig" "kubeconfig" {
   depends_on = [time_sleep.kubevip_wait]
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
   node                 = local.control_plane_endpoints[0]
-}
-
-
-data "talos_cluster_health" "final_health" {
-  depends_on = [data.talos_cluster_health.init_health, talos_cluster_kubeconfig.kubeconfig, helm_release.cilium, talos_machine_configuration_apply.worker]
-
-  client_configuration = data.talos_client_configuration.talosconfig.client_configuration
-  control_plane_nodes  = local.control_plane_endpoints
-  worker_nodes         = [for n in local.worker_nodes : n.ip_address]
-  endpoints            = local.control_plane_endpoints
 }
